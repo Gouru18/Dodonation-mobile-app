4582762329f75a470f41f71d429e45924e9a7356
Gouru18 <gouruchana.maghoo@umail.uom.ac.mu>
2026-04-28 15:24:34 +0400

    fixed db,register issues and deleted archive files

--- archive/chatbot/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ChatbotFAQ
-
-@admin.register(ChatbotFAQ)
-class ChatbotFAQAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer')
-    search_fields = ('question',)
--- archive/donor/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from archive.users.models import User
-
-class DonorProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="donor_profile")
-    donorID = models.AutoField(primary_key=True)
-
-    def __str__(self):
-        return f"Donor: {self.user.username}"
