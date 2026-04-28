ff2641af48f9e0ee8e844d4b3ca82bef53be3944
poshitabeersee <poshita.beersee@umail.uom.ac.mu>
2026-04-28 15:38:47 +0400

    Fix some errors

--- archive/chatbot/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ChatbotFAQ
-
-class ChatbotFAQSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ChatbotFAQ
-        fields = ['id', 'question', 'answer']
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = 'django-insecure-v=817h+1&2j!1k17l+axh2k3q^9#yx1b=wp#gd*xtbou((ij6o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.100.18", "127.0.0.1", "localhost"]
 
 AUTH_USER_MODEL = 'accounts.User'
 
